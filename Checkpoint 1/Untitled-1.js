94a7477b2b661339453b6327fd652e0ec9cb315f
salmaregaieg <52920035+salmaregaieg@users.noreply.github.com>
2019-07-19 10:17:24 +0200

    Add files via upload

---
+++ Checkpoint 1/Untitled-1.js
@@ -0,0 +1,18 @@
+function test(){
+    // event.defaultPrevented();
+document.getElementById("#textarea").style.fontWeight="bold";
+}
+function test1(){
+    // event.defaultPrevented();
+document.getElementById("#textarea").style.fontStyle="italic";
+}
+function test2(){
+    // event.defaultPrevented();
+document.getElementById("#textarea").style.textDecoration="Underline";
+}
+function test3(){
+    document.getElementById("#textarea").style.fontSize=document.getElementById("size").value
+}
+function test4(){
+    document.getElementById("#textarea").style.fontFamily=document.getElementById("ff").value
+}
